d3ba8c51f66c5ec09ea312a566236be38e55832c
yoqub-dev <149007168+yoqub-dev@users.noreply.github.com>
2025-11-23 12:37:20 +0500

    test supplier

--- src/main/java/com/example/hoddog/controller/SupplierController.java
+++ src/main/java/com/example/hoddog/controller/SupplierController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/suppliers")
+@RequestMapping("/api/supplierss")
 @RequiredArgsConstructor
 public class SupplierController {
 
